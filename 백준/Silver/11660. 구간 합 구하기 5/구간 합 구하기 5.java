6da5635d403b2cfbb9da3b9e2295606da459eab7
Kiseong Lee <73287840+dlrlseong@users.noreply.github.com>
2025-08-27 10:01:20 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 944 ms, Memory: 126252 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[][] arr = new int[N][N + 1];
+
+		for (int i = 0; i < N; i++) {
+			int sum = 0;
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				sum += Integer.parseInt(st.nextToken());
+				arr[i][j] = sum;
+			}
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken()) - 1;
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken()) - 1;
+			int y2 = Integer.parseInt(st.nextToken());
+			int result = 0;
+			for (int j = x1; j <= x2; j++) {
+				result += arr[j][y2] - arr[j][y1 - 1];
+			}
+			sb.append(result).append("\n");
+		}
+
+		System.out.println(sb);
+
+	}
+}
