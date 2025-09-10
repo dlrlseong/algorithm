990a2839304f7809650be9b2c8e1cfa27651e7ad
Kiseong Lee <73287840+dlrlseong@users.noreply.github.com>
2025-09-10 10:49:31 +0900

    [Gold II] Title: 색종이 붙이기, Time: 196 ms, Memory: 21484 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -2,69 +2,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int cnt[] = new int[1 + 5];
-	static int MIN = Integer.MAX_VALUE;
-	static int arr[][] = new int[10][10];
-	static boolean vis[][] = new boolean[10][10];
-	static boolean isPossible = false;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < 10; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < 10; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		dfs(0, 0);
-		int output = isPossible ? MIN : -1;
-		System.out.println(output);
-	}
-
-	public static void dfs(int r, int c) {
-		if (r == 10) {
-			isPossible = true;
-			MIN = Math.min(MIN, cnt[1] + cnt[2] + cnt[3] + cnt[4] + cnt[5]);
-			return;
-		}
-
-		if (cnt[1] + cnt[2] + cnt[3] + cnt[4] + cnt[5] > MIN)
-			return;
-
-		if (arr[r][c] == 0 || vis[r][c]) {
-			dfs(r + ((c + 1) / 10), (c + 1) % 10);
-			return;
-		}
-		for (int size = 5; size >= 1; size--) {
-			if (cnt[size] < 5 && isPossibleSize(r, c, size)) {
-				action(r, c, size, true);
-				dfs(r + ((c + 1) / 10), (c + 1) % 10);
-				action(r, c, size, false);
-			}
-		}
-	}
-
-	private static void action(int r, int c, int size, boolean go) {
-		cnt[size] += go ? 1 : -1;
-		for (int i = r; i < r + size; i++) {
-			for (int j = c; j < c + size; j++) {
-				vis[i][j] = go;
-			}
-		}
-
-//		System.out.println(r + " " + c + " " + size + " " + go + " " + cnt[size]);
-	}
-
-	private static boolean isPossibleSize(int r, int c, int size) {
-		for (int i = r; i < r + size; i++) {
-			for (int j = c; j < c + size; j++) {
-				if (i >= 10 || j >= 10 || vis[i][j])
-					return false;
-				if (arr[i][j] == 0)
-					return false;
-			}
-		}
-		return true;
-	}
+    static int[] cnt = new int[6];
+    static int MIN = Integer.MAX_VALUE;
+    static int[][] arr = new int[10][10];
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        for (int i = 0; i < 10; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 10; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        dfs(0, 0, 0);
+        System.out.println(MIN == Integer.MAX_VALUE ? -1 : MIN);
+    }
+    
+    public static void dfs(int r, int c, int usedPapers) {
+        // 조기 종료 - 원본과 동일한 위치
+        if (r == 10) {
+            MIN = Math.min(MIN, usedPapers);
+            return;
+        }
+        
+        // 가지치기 강화 - 더 앞으로 이동
+        if (usedPapers >= MIN) return;
+        
+        // 원본과 동일한 좌표 이동 방식 유지
+        if (arr[r][c] == 0) {
+            dfs(r + ((c + 1) / 10), (c + 1) % 10, usedPapers);
+            return;
+        }
+        
+        // 큰 크기부터 시도 - 원본과 동일
+        for (int size = 5; size >= 1; size--) {
+            if (cnt[size] < 5 && canPlace(r, c, size)) {
+                place(r, c, size, true);
+                dfs(r + ((c + 1) / 10), (c + 1) % 10, usedPapers + 1);
+                place(r, c, size, false);
+            }
+        }
+    }
+    
+    // vis 배열 없이 arr 직접 수정
+    private static void place(int r, int c, int size, boolean put) {
+        cnt[size] += put ? 1 : -1;
+        int value = put ? 0 : 1;
+        
+        for (int i = r; i < r + size; i++) {
+            for (int j = c; j < c + size; j++) {
+                arr[i][j] = value;
+            }
+        }
+    }
+    
+    private static boolean canPlace(int r, int c, int size) {
+        if (r + size > 10 || c + size > 10) return false;
+        
+        for (int i = r; i < r + size; i++) {
+            for (int j = c; j < c + size; j++) {
+                if (arr[i][j] != 1) return false;
+            }
+        }
+        return true;
+    }
 }
