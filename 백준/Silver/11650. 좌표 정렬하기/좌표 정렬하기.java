04d80f92205a8399d8a9a5314eb841b148d6af55
Kiseong Lee <73287840+dlrlseong@users.noreply.github.com>
2025-09-23 11:50:08 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 652 ms, Memory: 54132 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+		List<int[]> list = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+
+			list.add(new int[] { A, B });
+		}
+		Collections.sort(list, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[0] == o2[0])
+					return Integer.compare(o1[1], o2[1]);
+				return Integer.compare(o1[0], o2[0]);
+			}
+		});
+		for (int[] is : list) {
+			sb.append(is[0]).append(" ").append(is[1]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
